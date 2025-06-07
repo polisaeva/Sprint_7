e19ef5964f884b51f9e394e0718b667b80d14d44
Polina Isaeva <polly140893@yandex.ru>
2025-06-07 03:27:33 +0300

    added @Data, @AllArgsConstructor, @NoArgsConstructor

--- src/main/java/ru/practicum/Order.java
+++ src/main/java/ru/practicum/Order.java
@@ -1,7 +1,14 @@
 package ru.practicum;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Order {
     private String firstName;
     private String lastName;
@@ -12,93 +19,4 @@ public class Order {
     private String deliveryDate;
     private String comment;
     List<String> color;
-
-    //Конструктор, принимающий все поля класса Order
-    public Order(String firstName, String lastName, String address, int metroStation, String phone, int rentTime, String deliveryDate, String comment, List<String> color) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.address = address;
-        this.metroStation = metroStation;
-        this.phone = phone;
-        this.rentTime = rentTime;
-        this.deliveryDate = deliveryDate;
-        this.comment = comment;
-        this.color = color;
-    }
-
-    //Конструктор по умолчанию
-    public Order() {}
-
-    //Геттеры и сеттеры полей класса Order
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public int getMetroStation() {
-        return metroStation;
-    }
-
-    public void setMetroStation(int metroStation) {
-        this.metroStation = metroStation;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public int getRentTime() {
-        return rentTime;
-    }
-
-    public void setRentTime(int rentTime) {
-        this.rentTime = rentTime;
-    }
-
-    public String getDeliveryDate() {
-        return deliveryDate;
-    }
-
-    public void setDeliveryDate(String deliveryDate) {
-        this.deliveryDate = deliveryDate;
-    }
-
-    public String getComment() {
-        return comment;
-    }
-
-    public void setComment(String comment) {
-        this.comment = comment;
-    }
-
-    public List<String> getColor() {
-        return color;
-    }
-
-    public void setColor(List<String> color) {
-        this.color = color;
-    }
 }
